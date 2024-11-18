f6dff945c48a5da2201db28c728e808cdad7df99
Charlie Gao <charlie.gao@utexas.edu>
2024-11-18 17:58:03 -0600

    added team page to routes and jinja-fied the team template

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def index():
     md_template_string = markdown.markdown(homepage.read())
     return render_template('index.html',homepage_text=md_template_string)
 
+@app.route("/team")
+def team():
+    return render_template('team.html')
+
 if __name__ == "__main__":
     app.run()
